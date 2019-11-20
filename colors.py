10510fd608b51d16836be2541635bae820db987b
Ala770 <ala.alsanabani@gmail.com>
2019-11-20 11:23:05 -0500

    Initial commit, let's start it

---
+++ colors.py
@@ -0,0 +1,7 @@
+# this is firs git/push pycahrm project to github
+colors = ["red", "green", "Yellow", "blue"]
+i=0
+while i<len(colors):
+    print("When I was %d , my mode was just %s" %(i*10, colors[i]))
+    i= i+1
+
